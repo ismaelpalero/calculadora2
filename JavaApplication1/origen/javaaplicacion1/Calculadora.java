ad4c82773677067f0a02153d9d0c00a2b5664806
ismaelpalero <126110217+ismaelpalero@users.noreply.github.com>
2023-03-27 11:36:10 +0200

    comentario
    agrege comentario en principal

--- JavaApplication1/origen/javaaplicacion1/Calculadora.java
+++ JavaApplication1/origen/javaaplicacion1/Calculadora.java
@@ -4,10 +4,10 @@
  * and open the template in the editor.
  */
 package javaapplication1;
-
+/*Este es el primer paso
 /**
  *
- * @author redes
+ *  @author redes 
  */
 public class Calculadora {
     private int num1;
